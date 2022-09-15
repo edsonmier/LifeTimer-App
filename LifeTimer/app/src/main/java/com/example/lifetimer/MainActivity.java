828a884a71ad979bc65de22f803ff77857790bbb
edsonmier <edson_mier28@hotmail.com>
2022-09-15 02:56:14 -0500

    update: date button

--- LifeTimer/app/src/main/java/com/example/lifetimer/MainActivity.java
+++ LifeTimer/app/src/main/java/com/example/lifetimer/MainActivity.java
@@ -3,11 +3,42 @@ package com.example.lifetimer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.view.*;
+import android.app.DatePickerDialog;
+import android.widget.DatePicker;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText plannedDate;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        plannedDate = (EditText) findViewById(R.id.dateText);
+        plannedDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switch (view.getId()) {
+                    case R.id.dateText:
+                        showDatePickerDialog();
+                        break;
+                }
+            }
+        });
+    }
+
+    private void showDatePickerDialog() {
+        DatePickerFragment newFragment = DatePickerFragment.newInstance(new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                final String selectedDate = day + " / " + (month + 1) + " / " + year;
+                plannedDate.setText(selectedDate);
+            }
+        });
+
+        newFragment.show(getSupportFragmentManager(), "datePicker");
     }
 }
